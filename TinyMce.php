b863f23c7dafd124f5b995504992d6045ab004ef
Bogdan Savluk <savluk.bogdan@gmail.com>
2014-02-11 12:32:43 +0200

    4.0.16

--- TinyMce.php
+++ TinyMce.php
@@ -44,7 +44,7 @@ class TinyMce extends CInputWidget
         'fr_FR', 'gl', 'he_IL', 'hr', 'hu_HU', 'hy', 'id', 'it', 'ja', 'ka_GE', 'ko_KR', 'lb', 'lt', 'lv', 'nb_NO', 'nl',
         'pl', 'pt_BR', 'pt_PT', 'ro', 'ru', 'si_LK', 'sk', 'sl_SI', 'sr', 'sv_SE', 'ta', 'ta_IN', 'th_TH', 'tr_TR', 'ug', 'uk',
         'uk_UA', 'vi', 'vi_VN', 'zh_CN', 'zh_TW', 'en_GB', 'km_KH', 'tg', 'az', 'en_CA', 'is_IS',
-        'be', 'dv', 'kk', 'ml_IN',
+        'be', 'dv', 'kk', 'ml_IN', 'gd',
     ); // widget supported languages
 
 
